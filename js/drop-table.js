963f12897d59d2feb0c93df396e44af87237618b
JustinQinl <justinqinlin@gmail.com>
2024-03-20 06:26:08 -0400

    add drop-down selection boxes

--- js/drop-table.js
+++ js/drop-table.js
@@ -1,11 +1,14 @@
 function confirmDeletion() {
     var tableName = document.getElementById('tableName').value;
+    var dbName = document.getElementById('dbName').value;
+    var cluster = document.getElementById('cluster').value;
+
     if (tableName) {
-        var isConfirmed = confirm('Are you sure you want to delete the table: ' + tableName + '?');
+        var isConfirmed = confirm('Are you sure you want to delete the table: ' + tableName + ' in DB: ' + dbName + ', cluster: ' + cluster + '?');
         if (isConfirmed) {
             // Deletion logic
-            alert('Deletion process started for table: ' + tableName);
-            // Call a backend service to delete the table
+            alert('Deletion process started for table: ' + tableName + ' in DB: ' + dbName + ', cluster: ' + cluster);
+            // Call a backend service to delete the table, passing the tableName, dbName, and cluster
         }
     } else {
         alert('Please enter a table name.');
